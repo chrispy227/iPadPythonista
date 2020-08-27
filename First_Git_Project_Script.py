9b91e9ee4073e2739f51eec148e57e058a91f722
Christopher Bellocq <chbppp@gmail.com>
2020-08-27 06:48:18 +0300

    tutorial complete

---
+++ First_Git_Project_Script.py
@@ -0,0 +1,9 @@
+user_num1 = input("Input a number please: ")
+user_num2 = input("Input another number please: ")
+user_num3 = input("Input a third number please: ")
+ans = "Your Answer is "
+
+result = (int(user_num1) * (int(user_num2) + int(user_num3)))
+
+print(ans + str(result))
+
